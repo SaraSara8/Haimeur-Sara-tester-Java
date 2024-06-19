8be2de57bc83b4f605ffc4cb8b5553f86894c0fd
SaraSara8 <sara.haimeur84@gmail.com>
2024-06-19 08:20:27 +0200

    ajout assertEquals du test testParkingACar

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -4,6 +4,8 @@ import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -13,6 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -52,14 +56,29 @@ public class ParkingDataBaseIT {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        
+        // Il faut recurper le ticket de la base de donnée puis recuprer le spot et lire l'attribut Available
+        String vehicleRegNumber = "ABCDEF";
+        Ticket ticket = ticketDAO.getTicket(vehicleRegNumber);
+        ParkingSpot parkingSpot = ticket.getParkingSpot();
+        assertEquals(false, parkingSpot.isAvailable());
+        
+        
+        
     }
 
+   
     @Test
     public void testParkingLotExit(){
-        testParkingACar();
+       
+    	//testParkingACar();
+    	
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingService.processIncomingVehicle();
         parkingService.processExitingVehicle();
         //TODO: check that the fare generated and out time are populated correctly in the database
     }
 
+
+    
 }
